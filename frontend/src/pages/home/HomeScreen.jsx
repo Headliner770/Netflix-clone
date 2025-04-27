5de47e3b1e80af47ee4c62cd91e39ce038d0ca70
Headliner770 <paul248022@gmail.com>
2025-04-27 16:18:32 +0300

    go

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Navbar from "../../components/Navbar";
+import useGetTrendingContent from "../../hooks/useGetTrendingContent";
+import { Info, Play } from "lucide-react";
+import { useAuthStore } from "../../store/authUser";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  const { trendingContent } = useGetTrendingContent();
+
+  return (
+    <>
+      <div className="relative h-screen text-white bg-black">
+        <Navbar />
+
+        <img src="/extraction.png" alt="Hero img" className="absolute t" />
+      </div>
+      ;
+    </>
+  );
 };
 
 export default HomeScreen;
